a4043f6fc1561dd4c49a6587664dc8b500b2d32b
Will Keleher <will.keleher@gmail.com>
2013-10-28 14:46:41 -0700

    yeoman init

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global homeworkBuddy, $*/
+
+
+window.homeworkBuddy = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    homeworkBuddy.init();
+});
