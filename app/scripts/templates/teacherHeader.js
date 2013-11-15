2eec0fc0a918bba555d266e613499ef4972f76eb
Will Keleher <will.keleher@gmail.com>
2013-11-14 20:23:40 -0800

    haven't broken anything with css

--- app/scripts/templates/teacherHeader.js
+++
@@ -1,7 +0,0 @@
-homeworkBuddy.templates.teacherHeader = '\
-<div class = "header">\
-<a href = "#" class = "logout">Logout</a>\
-<button class = "createNew">Create New Homework!</button>\
-<button class = "grades">Grades!</button>\
-<div class = "hide assignments"><ul></ul></div>\
-</div>'
